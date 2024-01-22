7b3ca08d684a2777475b91604d49b07e2e86b233
Evie <idomin3craft@gmail.com>
2024-01-22 15:52:28 -0600

    Add verify method and make tests more than just todo!()

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
+#![allow(dead_code)]
 use std::fmt;
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub struct Guess {
     text: String,
 }
@@ -16,6 +17,7 @@ impl fmt::Display for GuessError<'_> {
 }
 
 impl Guess {
+    /// It is on the calling code to verify that text is in your dictionary
     /// # Errors
     /// Will return an error if given a string that isn't 5 letters.
     ///
@@ -58,8 +60,39 @@ impl Guess {
     pub const unsafe fn new(text: String) -> Self {
         Self { text }
     }
+
+    fn verify(&self, answer: Guess) -> GameResponse {
+        let mut resp = "".to_string();
+        for (i, guessed_char) in self.to_string().chars().enumerate() {
+            let answer_char = answer.text.chars().nth(i).unwrap();
+            if guessed_char == answer_char {
+                resp.push('G');
+            }
+        }
+        GameResponse { text: resp }
+    }
 }
+
 impl fmt::Display for Guess {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.text)
+    }
+}
+
+/// X for Grey, C for Green, Y for Yellow
+#[derive(Debug)]
+struct GameResponse {
+    text: String,
+}
+
+impl GameResponse {
+    /// X for Grey, C for Green, Y for Yellow
+    fn new(text: String) -> Self {
+        GameResponse { text }
+    }
+}
+
+impl fmt::Display for GameResponse {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self.text)
     }
@@ -82,22 +115,45 @@ mod tests {
     }
 
     #[test]
+    fn test_speed_speed() {
+        let speed = Guess::build("speed".to_string()).unwrap();
+        let resp: GameResponse = speed.verify(speed.clone());
+        assert_eq!(resp.text, "GGGGG");
+    }
+
+    #[test]
     fn test_speed_abide() {
-        todo!()
+        let speed = Guess::build("speed".to_string()).unwrap();
+        let abide = Guess::build("abide".to_string()).unwrap();
+
+        let resp: GameResponse = speed.verify(abide);
+        assert_eq!(resp.text, "XXYXX");
     }
 
     #[test]
     fn test_speed_erase() {
-        todo!()
+        let speed = Guess::build("speed".to_string()).unwrap();
+        let erase = Guess::build("erase".to_string()).unwrap();
+
+        let resp: GameResponse = speed.verify(erase);
+        assert_eq!(resp.text, "XXYYX");
     }
 
     #[test]
     fn test_speed_steal() {
-        todo!()
+        let speed = Guess::build("speed".to_string()).unwrap();
+        let steal = Guess::build("steal".to_string()).unwrap();
+
+        let resp: GameResponse = speed.verify(steal);
+        assert_eq!(resp.text, "GXGXX");
     }
 
     #[test]
     fn test_speed_crepe() {
-        todo!()
+        let speed = Guess::build("speed".to_string()).unwrap();
+        let crepe = Guess::build("crepe".to_string()).unwrap();
+
+        let resp: GameResponse = speed.verify(crepe);
+        assert_eq!(resp.text, "XXGYX");
     }
 }
